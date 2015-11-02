9d5276f6d0563e1f3fb59bfbc8e81ef6f0d4d7fb
Martin Panel <martin@xorax.info>
2015-11-02 17:17:45 +0100

    Fix wrong check keepDownloadedPatch option

--- mage-patch.php
+++ mage-patch.php
@@ -279,7 +279,7 @@ class PatchMage {
                 }
             }
             
-            if ($this->_keepDownloadedPatch) {
+            if (!$this->_keepDownloadedPatch) {
                 unlink($dir.$patchFile);
             }
         }
